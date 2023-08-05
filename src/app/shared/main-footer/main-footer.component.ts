68efc79b7ad835e5609f18e55f1a15e597c08ee0
Adnan <adnanghannam21@gmail.com>
2023-08-05 15:36:38 +0300

    fix main-footer icons

--- src/app/shared/main-footer/main-footer.component.ts
+++ src/app/shared/main-footer/main-footer.component.ts
@@ -12,22 +12,27 @@ export class MainFooterComponent {
   constructor() {
     this.features = [
       {
+        icon: "fa-solid fa-satellite-dish",
         title: "Fast Delivery",
         description: "Across West & East India"
       },
       {
+        icon: "fa-solid fa-money-check-dollar",
         title: "safe payment",
         description: "100% Secure Payment"
       },
       {
+        icon: "fa-regular fa-percent",
         title: "Online Discount",
         description: "Add Multi-buy Discount"
       },
       {
+        icon: "fa-regular fa-comments",
         title: "Help Center",
         description: "Dedicated 24/7 Support"
       },
       {
+        icon: "fa-regular fa-calendar-check",
         title: "Curated items",
         description: "From Handpicked Sellers"
       }
